f1c3b206f279e6133f9fbddb12126881a35b9c21
Otto Werse <otto.werse@web.de>
2020-03-31 10:00:53 +0200

    ESA1 working CLI

--- ESA1/stream.py
+++ ESA1/stream.py
@@ -1,29 +1,49 @@
+import threading
 import requests
 import click
 import time
 import sqlite3
 
-click.command()
-click.argument('stream_url')
-click.option('--duration', default=time.time() + 30)
-click.option('dateiname', default='myRadio.mp3')
+
+progress = None
 
 
+@click.command()
+@click.option('--stream_url', default='http://rbb-edge-2070.fra-lg.cdn.addradio.net/rbb/fritz/live/mp3/128/stream.mp3')
+@click.option('--dateiname', default='myRadio.mp3')
+@click.option('--duration', default=30)
 def record(stream_url, duration, dateiname):
+    # click.echo("{}, {}, {}".format(stream_url, duration, dateiname))
     with open(dateiname, 'wb') as f:
+        global progress
+        progress = duration
+        print_progress()
+
         response = requests.get(stream_url, stream=True)
 
         current = time.time()
         target = current + duration
         while current <= target:
-            print(f'Current: {current}, Target: {target}')
             current = time.time()
             c = next(response.iter_content())
             f.write(c)
         response.close()
+        print('Done!')
     # print(f'result: {result}')
 
 
+def print_progress():
+    global progress
+    if progress >= 1:
+        print(progress)
+        progress -= 1
+        threading.Timer(1.0, print_progress).start()
+
+
+def printit():
+    threading.Timer(5.0, printit).start()
+    print("Hello, World!")
+
+
 if __name__ == '__main__':
-    stream_link = 'http://rbb-edge-2070.fra-lg.cdn.addradio.net/rbb/fritz/live/mp3/128/stream.mp3'
-    record(stream_link, 5, 'test.mp3')
+    record()
